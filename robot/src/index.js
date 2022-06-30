a8a257b226d5ad69b11092026b3b870c3bea0cda
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-30 23:38:50 +0500

    reported

--- robot/src/index.js
+++ robot/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+
 import "tachyons";
 import App from "./container/App";
 
