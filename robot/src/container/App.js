739363118aca2929b203fd0a07a91d3f556522c7
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-30 23:09:47 +0500

    practicum usememo

--- robot/src/container/App.js
+++ robot/src/container/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState , useMemo } from "react";
+import ChildMessages from "../components/ChildMessages";
 
 import "./App.css";
 const App = () => {
@@ -7,15 +8,18 @@ const App = () => {
   const AddMessage = () => {
     setMessages((prev) => prev + 1);
   };
-  const RemoveMessage = () => {
-    setMessages((prev) => prev - 1);
-  };
+
+  useMemo(() => {
+    return ChildMessages
+  } , [])
   return (
     <div className="container">
       <div className="card">
         <h1>Messages : {messages}</h1>
-        <button className="btn circle" onClick={AddMessage}>Add</button>
-        <button className="btn" onClick={RemoveMessage}>Remove</button>
+        <ChildMessages />
+        <button className="btn circle" onClick={AddMessage}>
+          Add
+        </button>
       </div>
     </div>
   );
