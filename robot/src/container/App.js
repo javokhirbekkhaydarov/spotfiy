1ea8c6e90364812a528ff3185af1c27fa0eaa860
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-01 23:15:27 +0500

    added new css codes

--- robot/src/container/App.js
+++ robot/src/container/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useMemo } from "react";
 import Player from "../components/Player";
 import Song from "../components/Song";
-
+import "../styles/app.scss"
 import "./App.css";
 
 const App = () => {
