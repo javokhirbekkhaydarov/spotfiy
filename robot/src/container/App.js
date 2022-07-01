91b7b078997b4097f52b38ad04deb4fba2471685
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-01 19:59:21 +0500

    added Player and Song js

--- robot/src/container/App.js
+++ robot/src/container/App.js
@@ -1,12 +1,11 @@
-import React, { useState , useMemo } from "react";
+import React, { useState, useMemo } from "react";
+import Player from "../components/Player";
+import Song from "../components/Song";
 
 import "./App.css";
 
 const App = () => {
- 
-  return (
-  <h1>ohoy</h1>
-  )
+  return (<Song />), (<Player />);
 };
 
 export default App;
