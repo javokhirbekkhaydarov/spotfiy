754a9adb9219426049bdd9122487a7ac2ca2a984
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-02 18:42:40 +0500

    added new music that is data js

---
+++ robot/src/data.js
@@ -0,0 +1,87 @@
+import { v4 as uuidv4 } from "uuid";
+function util() {
+  return [
+    {
+      name: "Sleepy Fish, coa",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/3aaa079444cf9c1d4e0a1f53947321b29ff554c0-1024x1024.jpg",
+      artist: "A Reminder",
+      color: ["#ccc444", "#754551"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Full Circle",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/c0c6175640eb9aa14b955ae91e255b3175de1c32-1024x1024.jpg",
+      artist: "RELEASES",
+      color: ["#ccc444", "#754551"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Smile High, Richard Vagner",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/5158b15bf8cb3b9d50588cfc31d36d28c816dc0c-1024x1024.jpg",
+      artist: "Rooftop Breeze",
+      color: ["#ccc444", "#754551"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Full Circle",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/42878bcb02c1a3613f4913b226674cd36bac9830-1024x1024.jpg",
+      artist: "RELEASES",
+      color: ["#ccc444", "#754551"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Es-K, Danny James, John Culbreth",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/2b9d337d372db542748e2643dc90af62dd3418e2-1024x1024.jpg",
+      artist: "Nectarine",
+      color: ["#ccc444", "#754551"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Full Circle",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/5158b15bf8cb3b9d50588cfc31d36d28c816dc0c-1024x1024.jpg",
+      artist: "RELEASES",
+      color: ["#ccc444", "#754551"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Sleepy Fish, coa",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/3aaa079444cf9c1d4e0a1f53947321b29ff554c0-1024x1024.jpg",
+      artist: "A Reminder",
+      color: ["#ccc444", "#754551"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Full Circle",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/c0c6175640eb9aa14b955ae91e255b3175de1c32-1024x1024.jpg",
+      artist: "RELEASES",
+      color: ["#ccc444", "#754551"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default util;
