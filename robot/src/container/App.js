24c85529a30b55f8b7e650b0ed8ae15d6ca1cc19
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-02 20:15:26 +0500

    added play button and working

--- robot/src/container/App.js
+++ robot/src/container/App.js
@@ -8,11 +8,17 @@ import data from "../data";
 const App = () => {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
-    <>
-      <Song currentSong={currentSong}/> <Player />
-    </>
+    <div className="App">
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
+    </div>
   );
 };
 
