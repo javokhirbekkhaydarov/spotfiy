45ba5b73d5d3d7468059574471c2246435dd1eac
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-03 07:11:25 +0500

    select music

--- robot/src/components/LibrarySong.js
+++ robot/src/components/LibrarySong.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song  ,  setCurrentSong}) => {
+
+  const songSelectHandler = () => {
+    setCurrentSong(song)
+  }
   return (
-    <div className="library-song">
+    <div onClick={songSelectHandler} className="library-song">
       <img src={song.cover} alt={song.name} />
       <div className="song-description">
         <h2>{song.name}</h2>
