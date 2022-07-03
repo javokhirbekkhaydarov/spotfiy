7cacfa352185f548fb34797fc421d911a957d8df
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-03 12:09:31 +0500

    added nav toggle

--- robot/src/container/App.js
+++ robot/src/container/App.js
@@ -2,13 +2,14 @@ import React, { useState, useRef } from "react";
 import Player from "../components/Player";
 import Song from "../components/Song";
 import "../styles/app.scss";
-import "./App.css";
 import Library from "../components/Library";
 import data from "../data";
+import Nav from "../components/Nav";
 const App = () => {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
+ const [libraryStatus, setLibraryStatus] = useState(false)
   // Ref
   const audioRef = useRef(null);
 
@@ -33,6 +34,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <Nav  libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
       <Song currentSong={currentSong} />
       <Player
         currentSong={currentSong}
@@ -48,6 +50,7 @@ const App = () => {
         setCurrentSong={setCurrentSong}
         isPlaying={isPlaying}
         setSongs={setSongs}
+        libraryStatus={libraryStatus}
       />
     </div>
   );
