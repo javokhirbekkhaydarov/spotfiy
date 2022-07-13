c3fa8eddc8aaada2909baef5aa39067b9503d0bd
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-07-13 09:04:41 +0500

    addded new branch and added new component

--- robot/src/container/App.js
+++ robot/src/container/App.js
@@ -5,6 +5,7 @@ import "../styles/app.scss";
 import Library from "../components/Library";
 import data from "../data";
 import Nav from "../components/Nav";
+import UseLayoutEffect from "../components/UseLayoutEffect/UseLayoutEffect";
 const App = () => {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
@@ -33,29 +34,35 @@ const App = () => {
   });
 
   return (
-    <div className={`App ${libraryStatus? "library-active" : ""}`}>
-      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
-      <Song currentSong={currentSong} />
-      <Player
-        currentSong={currentSong}
-        isPlaying={isPlaying}
-        audioRef={audioRef}
-        setIsPlaying={setIsPlaying}
-        setSongInfo={setSongInfo}
-        songInfo={songInfo}
-        timeUpdateHandler={timeUpdateHandler}
-        songs={songs}
-        setCurrentSong={setCurrentSong}
-        setSongs={setSongs}
-      />
-      <Library
-        songs={songs}
-        setCurrentSong={setCurrentSong}
-        isPlaying={isPlaying}
-        setSongs={setSongs}
-        libraryStatus={libraryStatus}
-      />
-    </div>
+    (<UseLayoutEffect />),
+    (
+      <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+        <Nav
+          libraryStatus={libraryStatus}
+          setLibraryStatus={setLibraryStatus}
+        />
+        <Song currentSong={currentSong} />
+        <Player
+          currentSong={currentSong}
+          isPlaying={isPlaying}
+          audioRef={audioRef}
+          setIsPlaying={setIsPlaying}
+          setSongInfo={setSongInfo}
+          songInfo={songInfo}
+          timeUpdateHandler={timeUpdateHandler}
+          songs={songs}
+          setCurrentSong={setCurrentSong}
+          setSongs={setSongs}
+        />
+        <Library
+          songs={songs}
+          setCurrentSong={setCurrentSong}
+          isPlaying={isPlaying}
+          setSongs={setSongs}
+          libraryStatus={libraryStatus}
+        />
+      </div>
+    )
   );
 };
 
